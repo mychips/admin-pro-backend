cd4d3ef3b500bbccfa28ce53384ebcd98714cff8
mychips <mychip13@hotmail.com>
2022-11-19 01:38:38 -0600

    Backend con auth

---
+++ controllers/auth.js
@@ -0,0 +1,52 @@
+const { response } = require('express');
+const { generarJWT } = require('../helpers/jwt')
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario')
+
+const login = async( req, res = response ) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar email
+        const usuarioDB = await Usuario.findOne({ email });
+
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Constraseña no valida o email no valida'
+            });
+        }
+
+        // Verificar contraseña
+        const validPassword = bcrypt.compareSync( password, usuarioDB.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Constraseña no valida o email no valida'
+            });
+        }
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuarioDB.id );
+
+        res.json({
+            ok: true,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+}
+
+
+module.exports = {
+    login
+}
--- index.js
+++ index.js
@@ -11,18 +11,18 @@ const app = express();
 // Configurar CORS
 app.use( cors() );
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Base de datos
 dbConnection();
 
 // console.log( process.env );
 
 // Rutas
-app.get( '/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
-})
+app.use( '/api/usuarios', require('./routes/usuarios'));
+app.use( '/api/login', require('./routes/auth'));
+
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor en el puerto ' + process.env.PORT);
